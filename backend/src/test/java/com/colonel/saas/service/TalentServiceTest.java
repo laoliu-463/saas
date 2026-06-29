f12bb60301d5f88ae94d16e09c5007628cc28e7b
LaoLiu <caojianing463@gmail.com>
2026-06-29 16:22:19 +0800

    docs: add ddd completion evidence matrix

--- backend/src/test/java/com/colonel/saas/service/TalentServiceTest.java
+++ backend/src/test/java/com/colonel/saas/service/TalentServiceTest.java
@@ -11,6 +11,7 @@ import com.colonel.saas.common.exception.ForbiddenException;
 import com.colonel.saas.domain.talent.application.TalentBatchImportApplicationService;
 import com.colonel.saas.domain.talent.application.TalentClaimApplicationService;
 import com.colonel.saas.domain.talent.application.TalentEnrichmentApplicationService;
+import com.colonel.saas.domain.talent.application.ExclusiveTalentCheckApplicationService;
 import com.colonel.saas.domain.talent.application.TalentPageApplicationService;
 import com.colonel.saas.domain.order.facade.OrderReadFacade;
 import com.colonel.saas.domain.sample.facade.SampleDomainFacade;
@@ -119,6 +120,7 @@ class TalentServiceTest {
                 newTalentPoolApplicationService(),
                 newTalentEnrichmentApplicationService(),
                 newTalentPageApplicationService(),
+                newExclusiveTalentCheckApplicationService(),
                 newTalentClaimApplicationService(),
                 operationLogService,
                 userDomainFacade,
@@ -876,6 +878,7 @@ class TalentServiceTest {
                 newTalentPoolApplicationService(),
                 newTalentEnrichmentApplicationService(),
                 newTalentPageApplicationService(),
+                newExclusiveTalentCheckApplicationService(),
                 newTalentClaimApplicationService(),
                 operationLogService,
                 userDomainFacade,
@@ -1374,6 +1377,7 @@ class TalentServiceTest {
                 newTalentPoolApplicationService(),
                 newTalentEnrichmentApplicationService(),
                 newTalentPageApplicationService(),
+                newExclusiveTalentCheckApplicationService(dataScopePolicy, properties),
                 newTalentClaimApplicationService(dataScopePolicy, properties),
                 operationLogService,
                 userDomainFacade,
@@ -1416,6 +1420,22 @@ class TalentServiceTest {
                 talentClaimMapper,
                 new DataScopePolicy(),
                 new DddRefactorProperties());
+    }
+
+    private ExclusiveTalentCheckApplicationService newExclusiveTalentCheckApplicationService() {
+        return newExclusiveTalentCheckApplicationService(new DataScopePolicy(), new DddRefactorProperties());
+    }
+
+    private ExclusiveTalentCheckApplicationService newExclusiveTalentCheckApplicationService(
+            DataScopePolicy dataScopePolicy,
+            DddRefactorProperties properties) {
+        return new ExclusiveTalentCheckApplicationService(
+                talentMapper,
+                orderReadFacade,
+                sampleDomainFacade,
+                configDomainFacade,
+                dataScopePolicy,
+                properties);
     }
 
     private TalentClaimApplicationService newTalentClaimApplicationService() {
